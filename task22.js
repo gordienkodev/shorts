b5ca17f23629bf1a1b5955d65d7b62c575d0fe13
Pavel Gordienko <guzmo@yandex.ru>
2026-06-16 12:33:40 +0300

    feat: add 22 task

---
+++ task22.js
@@ -0,0 +1,15 @@
+// что будет в консоли и почему
+// 
+function changeStuff(a, b)
+{
+    a = a * 10;
+    b.item = "changed";
+}
+
+let num = 10;
+let obj1 = { item: "unchanged" };
+
+changeStuff(num, obj1);
+
+console.log(num);
+console.log(obj1.item);
