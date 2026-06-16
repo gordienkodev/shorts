d5d5cf0d2a37142bbf6c88d2e9643f14da771dfb
Pavel Gordienko <guzmo@yandex.ru>
2026-06-16 12:56:52 +0300

    feat: add 23 task

---
+++ task23.js
@@ -0,0 +1,14 @@
+// что будет в консоли и почему
+
+const shape = {
+  radius: 10,
+
+  diameter() {
+    return this.radius * 2;
+  },
+
+  perimeter: () => 2 * this.radius
+};
+
+console.log(shape.diameter());
+console.log(shape.perimeter());
