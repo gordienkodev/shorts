d15a0a19504102043acd5f454f2c1fdc135b6ed5
Pavel Gordienko <guzmo@yandex.ru>
2026-06-25 03:09:09 +0300

    feat: add 30 task

---
+++ task30.js
@@ -0,0 +1,18 @@
+let num = 10;
+
+const arr = [num];
+
+num = null;
+
+console.log(arr); // [10]
+
+
+
+let person = { name: "John" };
+
+const members = [person];
+
+person = null;
+
+console.log(members); 
+// [{ name: "John" }]
