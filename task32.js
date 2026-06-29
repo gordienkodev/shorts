bf2619128f5e12ab873fad9ffcf5614482899fac
Pavel Gordienko <guzmo@yandex.ru>
2026-06-29 03:43:58 +0300

    fix: answers

--- task32.js
+++ task32.js
@@ -1,12 +1,14 @@
-JSON.stringify(undefined);      // 
-JSON.stringify(function(){});   // 
+JSON.stringify(undefined);      
+// undefined
+JSON.stringify(function(){});  
+// undefined
 
 JSON.stringify(
   [1, undefined, function(){}, 4]
 );
-// 
+// "[1,null,null,4]"
 
 JSON.stringify(
   { a: 2, b: function(){} }
 );
-// 
+// '{"a":2}'
