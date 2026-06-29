36f306190bc1947edcc089f100ead079879d55f7
Pavel Gordienko <guzmo@yandex.ru>
2026-06-30 02:27:03 +0300

    feat: add task 33

---
+++ task33.js
@@ -0,0 +1,46 @@
+function A({ children }) {
+  console.log('A')
+
+  const [state, setState] = useState(0)
+
+  useEffect(() => {
+    setState(state => state + 1)
+  }, [])
+
+  return children
+}
+
+function B() {
+  console.log('B')
+
+  return <C />
+}
+
+function C() {
+  console.log('C')
+
+  return null
+}
+
+function D() {
+  console.log('D')
+
+  return null
+}
+
+function App() {
+  console.log('App')
+
+  return (
+    <div>
+      <A><B /></A>
+      <D />
+    </div>
+  )
+}
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(<App />)
+
+// App A B C D A
