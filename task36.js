e0ed9d1f8e8be6a2b4a2d0b6372c7d9278ca869a
Pavel Gordienko <guzmo@yandex.ru>
2026-07-03 02:43:05 +0300

    feat: add task 36

---
+++ task36.js
@@ -0,0 +1,17 @@
+foo();
+
+function foo() {
+    let b = 10;
+
+    function b() {
+        return 5;
+    }
+
+    console.log(b());
+
+    function b() {
+        return 15;
+    }
+}
+
+// Uncaught SyntaxError: Identifier 'b' has already been declared
