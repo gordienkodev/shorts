c84b2b1f19b28d879d7224c28b82b945a66eee77
Pavel Gordienko <guzmo@yandex.ru>
2026-07-02 03:18:35 +0300

    feat: add task 35

---
+++ task35.js
@@ -0,0 +1,43 @@
+while (true) {
+  console.log('start');
+
+  window.setTimeout(() => {
+    console.log('timeout');
+  }, 0);
+
+  window.requestAnimationFrame &&
+    window.requestAnimationFrame(() => {
+      console.log('raf');
+    });
+
+  window.requestIdleCallback &&
+    window.requestIdleCallback(() => {
+      console.log('ric');
+    });
+
+  Promise.resolve()
+    .then(() => {
+      console.log('promise foo');
+    })
+    .then(() => {
+      console.log('promise bar');
+    });
+
+  const promise = new Promise((resolve, reject) => {
+    console.log('promise baz');
+    resolve(true);
+  });
+
+  promise.then(() => {
+    console.log('promise fez');
+  });
+
+  console.log('finish');
+}
+
+// start
+// promise baz
+// finish
+// start
+// promise baz
+// finish
