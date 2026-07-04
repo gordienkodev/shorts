ef7609eab14e7e6abd3f388d695df0633a06695f
Pavel Gordienko <guzmo@yandex.ru>
2026-07-04 03:50:35 +0300

    feat: add task 38

---
+++ task38.js
@@ -0,0 +1,17 @@
+let value = 2;
+
+function showValue() {
+    console.log(`showValue ${value}`);
+}
+
+function wrapper() {
+
+
+    console.log(`wrapper ${value}`);
+
+    showValue();
+    
+    var value = 3;
+}
+
+wrapper();
