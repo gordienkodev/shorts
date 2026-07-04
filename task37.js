b816ba09cf458b04e572bcac3f364341ab584e83
Pavel Gordienko <guzmo@yandex.ru>
2026-07-04 03:22:55 +0300

    feat: add task 37

---
+++ task37.js
@@ -0,0 +1,19 @@
+function l() {
+    console.log(c);
+    var c = 5;
+
+    setTimeout(function () {
+        console.log(c);
+        var c = 10;
+        console.log(c);
+    }, 100);
+
+    console.log(c);
+}
+
+l(); 
+
+// undefined
+// 5
+// undefined
+// 10
