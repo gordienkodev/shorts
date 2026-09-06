9b03dcfa9602c62d27bba079f08e49d1c47712e1
Pavel Gordienko <guzmo@yandex.ru>
2026-09-06 04:44:16 +0300

    fix: clear solution

--- tasks/task71.ts
+++ tasks/task71.ts
@@ -5,6 +5,6 @@
 /**
  * Task 6: Address the TypeScript error in the following generic function.
  */
-function greet<T extends { name: string }>(entity: T): string {
+function greet(entity: T): string {
   return `Hello, ${entity.name}!`;
 }
